743c7997c5e4ce92b2ff64f01db3da6197e020cd
Michal <polonecmichal36@gmail.com>
2026-09-23 09:30:00 +0200

    Update benfordcovid.py

--- benfordcovid.py
+++ benfordcovid.py
@@ -1,1 +1,11 @@
 #du dorobit, zisti ci ten weekly death zodpovedaju benfordovmu zakonu, csv
+import csv
+cislo = {}
+for i in range(1,10):
+    cislo[i] = 0
+txt = open("D:\\škola\\Downloads\\data.csv")
+for line in txt:
+    death = line[18:19] #death neni univerzalne treba spravit 
+    cislo[death] += 1
+print(cislo)
+
