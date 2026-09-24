ae30e5cfa2d939a8cf1e03a48a5aa8f636512cbb
Michal <polonecmichal36@gmail.com>
2026-09-24 08:26:06 +0200

    Refactor Benford's Law analysis for weekly deaths
    Refactor code to read death data from CSV and count occurrences based on Benford's Law. Update file path and improve data processing logic.

--- benfordcovid.py
+++ benfordcovid.py
@@ -1,11 +1,20 @@
 #du dorobit, zisti ci ten weekly death zodpovedaju benfordovmu zakonu, csv
 import csv
 cislo = {}
+count = 0
 for i in range(1,10):
     cislo[i] = 0
-txt = open("D:\\škola\\Downloads\\data.csv")
+txt = open("/home/polonec/Downloads/data.csv")
 for line in txt:
-    death = line[18:19] #death neni univerzalne treba spravit 
-    cislo[death] += 1
-print(cislo)
+    count += 1
+    riadok = line.split(',')
+    death = riadok[5]
+    if death.isnumeric() and int(death[0]) > 0:
+        prvy = int(death[0])
+        cislo[prvy] += 1
+print(cislo, count)
 
+for i in cislo:
+    percenta = (cislo[i]/count)*100
+    ans = str(percenta) + '%' + ' '
+    print(ans)
